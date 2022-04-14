67b2eab6c2f3ca044909746e6bfcb44dda56bf32
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 21:15:14 +0300

    feat: add creational pattern - prototype

---
+++ src/Creational/TeachMeSkills.DotNet.Patterns.Creational.Prototype/IPrototype.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace TeachMeSkills.DotNet.Patterns.Creational.Prototype
+{
+    /// <summary>
+    /// Prototype interface.
+    /// </summary>
+    /// <typeparam name="T">Generic result type from method.</typeparam>
+    public interface IPrototype<T>
+    {
+        /// <summary>
+        /// Deep copy by json serialization.
+        /// </summary>
+        public T DeepCopyByJson()
+        {
+            var jsonObject = JsonSerializer.Serialize(this);
+            var result = JsonSerializer.Deserialize<T>(jsonObject);
+
+            return result;
+        }
+    }
+}
