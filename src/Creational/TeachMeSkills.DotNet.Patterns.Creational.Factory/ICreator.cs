4de6c09bdbb7aeee78d3ad29300f822b7318b79e
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 20:59:16 +0300

    feat: add creational pattern - factory

---
+++ src/Creational/TeachMeSkills.DotNet.Patterns.Creational.Factory/ICreator.cs
@@ -0,0 +1,14 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Creational.Factory
+{
+    /// <summary>
+    /// Creator.
+    /// </summary>
+    /// <typeparam name="T">Generic result type from method.</typeparam>
+    public interface ICreator<T>
+    {
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        public T Create();
+    }
+}
---
+++ src/Creational/TeachMeSkills.DotNet.Patterns.Creational.Factory/IProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Creational.Factory
+{
+    /// <summary>
+    /// Product.
+    /// </summary>
+    /// <typeparam name="T">Generic result type from method.</typeparam>
+    public interface IProduct<T>
+    {
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        public T Get();
+    }
+}
