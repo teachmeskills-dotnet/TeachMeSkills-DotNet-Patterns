9cced1ebbbc1bac86c339598e81c0c13ecf3741c
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 21:33:03 +0300

    feat: add structural pattern - adapter

---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Adapter/IAdapter.cs
@@ -0,0 +1,27 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Adapter
+{
+    /// <summary>
+    /// Adapter interface.
+    /// </summary>
+    /// <typeparam name="T">Source generic type.</typeparam>
+    /// <typeparam name="V">Generic result type from method.</typeparam>
+    /// <typeparam name="Z">Generic result type from method.</typeparam>
+    public interface IAdapter<T, V, Z>
+    {
+        /// <summary>
+        /// Souce.
+        /// </summary>
+        public T Source { get; }
+
+        /// <summary>
+        /// Get source data.
+        /// </summary>
+        /// <param name="source">Source data.</param>
+        public V GetSource(T source);
+
+        /// <summary>
+        /// Result.
+        /// </summary>
+        public Z Result();
+    }
+}
