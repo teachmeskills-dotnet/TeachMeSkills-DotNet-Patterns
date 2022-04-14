7025a637fb53b52482fffa92802ba898b8e9ef0a
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 21:25:00 +0300

    feat: add creational pattern - singleton

---
+++ src/Creational/TeachMeSkills.DotNet.Patterns.Creational.Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Creational.Singleton
+{
+    // UNDONE: how to use?
+
+    /// <summary>
+    /// Singleton.
+    /// </summary>
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        /// <summary>
+        /// Get instance.
+        /// </summary>
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
