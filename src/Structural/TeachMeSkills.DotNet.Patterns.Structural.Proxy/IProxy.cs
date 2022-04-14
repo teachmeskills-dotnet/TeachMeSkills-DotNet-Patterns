d7f4237bed7bcdd1ff9b82c01eb0d22c1c181c0f
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 22:05:37 +0300

    feat: add structural pattern - proxy

---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Proxy/IProxy.cs
@@ -0,0 +1,20 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Proxy
+{
+    /// <summary>
+    /// Proxy interface.
+    /// </summary>
+    /// <typeparam name="T">Source generic type.</typeparam>
+    /// <typeparam name="V">Generic result type from method.</typeparam>
+    public interface IProxy<T, V>
+    {
+        /// <summary>
+        /// Source.
+        /// </summary>
+        public T Source { get; }
+
+        /// <summary>
+        /// Get result.
+        /// </summary>
+        public V GetResult();
+    }
+}
