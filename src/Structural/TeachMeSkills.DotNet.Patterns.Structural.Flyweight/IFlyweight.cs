e93ba33ffc2f3614a3af55167073db89ec1b9200
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 22:03:26 +0300

    feat: add structural pattern - flyweight

---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Flyweight/IFlyweight.cs
@@ -0,0 +1,22 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Flyweight
+{
+    // UNDONE: change void -> V
+
+    /// <summary>
+    /// Flyweight interface.
+    /// </summary>
+    /// <typeparam name="T">Source generic type.</typeparam>
+    public interface IFlyweight<T>
+    {
+        /// <summary>
+        /// Source.
+        /// </summary>
+        public T Source { get; }
+
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        /// <param name="uniqueState">Unique state.</param>
+        public void Operation(T uniqueState);
+    }
+}
---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Flyweight/IFlyweightFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Flyweight
+{
+    // UNDONE: get XML comment to class
+
+    /// <summary>
+    /// Flyweight factory interface.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public interface IFlyweightFactory<T, V>
+    {
+        public IList<Tuple<IFlyweight<T>, V>> Flyweights { get; }
+
+        public string GetKey(T key);
+
+        public IFlyweight<T> GetFlyweight(T sharedState);
+
+        public void ListFlyweights();
+    }
+}
