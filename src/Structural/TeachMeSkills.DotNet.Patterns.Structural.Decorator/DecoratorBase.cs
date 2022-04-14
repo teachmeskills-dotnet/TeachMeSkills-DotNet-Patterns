199698868bfda2304ac2ff42ddcd6e2fb1476ed5
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 21:56:39 +0300

    feat: add structural pattern - decorator

---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Decorator/DecoratorBase.cs
@@ -0,0 +1,48 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Decorator
+{
+    // UNDONE: change void to V
+
+    /// <summary>
+    /// Decorator base class.
+    /// </summary>
+    /// <typeparam name="T">Generic result type from DecoratorComponentBase method.</typeparam>
+    public abstract class DecoratorBase<T, V> : DecoratorComponentBase<T>
+    {
+        /// <summary>
+        /// Decorator component.
+        /// </summary>
+        protected DecoratorComponentBase<T> _component;
+
+        /// <summary>
+        /// Constructor with params.
+        /// </summary>
+        /// <param name="component">Decorator component.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public DecoratorBase(DecoratorComponentBase<T> component)
+        {
+            _component = component ?? throw new ArgumentNullException(nameof(component));
+        }
+
+        /// <summary>
+        /// Set component.
+        /// </summary>
+        /// <param name="component">Decorator component</param>
+        public void SetComponent(DecoratorComponentBase<T> component)
+        {
+            _component = component ?? throw new ArgumentNullException(nameof(component));
+        }
+
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        public override T Operation()
+        {
+            if (_component is not null)
+            {
+                return _component.Operation();
+            }
+
+            return default;
+        }
+    }
+}
---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Decorator/DecoratorComponentBase.cs
@@ -0,0 +1,14 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Decorator
+{
+    /// <summary>
+    /// Decorator component base class.
+    /// </summary>
+    /// <typeparam name="T">Generic result type from method.</typeparam>
+    public abstract class DecoratorComponentBase<T>
+    {
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        public abstract T Operation();
+    }
+}
