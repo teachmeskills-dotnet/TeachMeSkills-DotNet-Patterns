17be82b37834bd54aabf994bf7ee5a10b6121897
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 21:42:08 +0300

    feat: add structural pattern - bridge

---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Bridge/BridgeBase.cs
@@ -0,0 +1,28 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Bridge
+{
+    /// <summary>
+    /// Abstraction bridge base class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BridgeBase<T>
+    {
+        /// <summary>
+        /// Bridge interface.
+        /// </summary>
+        protected IBridge _implementation;
+
+        /// <summary>
+        /// Custructor with params.
+        /// </summary>
+        /// <param name="implementation">Bridge interface.</param>
+        public BridgeBase(IBridge implementation)
+        {
+            _implementation = implementation ?? throw new ArgumentNullException(nameof(implementation));
+        }
+
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        public abstract T Operation();
+    }
+}
---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Bridge/IBridge.cs
@@ -0,0 +1,16 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Bridge
+{
+    // UNDONE: change Task -> V
+
+    /// <summary>
+    /// Bridge interface.
+    /// </summary>
+    public interface IBridge
+    {
+        /// <summary>
+        /// Operation.
+        /// </summary>
+        /// <returns></returns>
+        Task Operation();
+    }
+}
