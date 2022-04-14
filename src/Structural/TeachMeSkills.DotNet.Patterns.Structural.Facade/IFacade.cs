85d50723fca066529b6c2dd2758cecef00a8041c
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 22:00:26 +0300

    feat: add structural pattern - facade

---
+++ src/Structural/TeachMeSkills.DotNet.Patterns.Structural.Facade/IFacade.cs
@@ -0,0 +1,26 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Structural.Facade
+{
+    /// <summary>
+    /// Facade interface.
+    /// </summary>
+    /// <typeparam name="T">SubSystem1 generic type.</typeparam>
+    /// <typeparam name="V">SubSystem2 generic type.</typeparam>
+    /// <typeparam name="Z">Generic result type from method.</typeparam>
+    public interface IFacade<T, V, Z>
+    {
+        /// <summary>
+        /// SubSystem1.
+        /// </summary>
+        public T SubSystem1 { get; }
+
+        /// <summary>
+        /// SubSystem2.
+        /// </summary>
+        public V SubSystem2 { get; }
+
+        /// <summary>
+        /// Get result.
+        /// </summary>
+        public Z GetResult();
+    }
+}
