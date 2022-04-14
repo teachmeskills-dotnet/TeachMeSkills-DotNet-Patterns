5f70744460dc325fff81d1df4cdaf6a52e1a609a
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-14 21:11:17 +0300

    feat: add creational pattern - builder

---
+++ src/Creational/TeachMeSkills.DotNet.Patterns.Creational.Builder/IBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Creational.Builder
+{
+    // UNDONE: change Task -> V
+
+    /// <summary>
+    /// Builder interface.
+    /// </summary>
+    /// <typeparam name="T">Product generic type.</typeparam>
+    public interface IBuilder<T>
+    {
+        /// <summary>
+        /// Product.
+        /// </summary>
+        T Product { get; }
+
+        /// <summary>
+        /// Build.
+        /// </summary>
+        public Task BuildAsync();
+    }
+}
---
+++ src/Creational/TeachMeSkills.DotNet.Patterns.Creational.Builder/IDirector.cs
@@ -0,0 +1,26 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Creational.Builder
+{
+    // UNDONE: change Task -> V
+
+    /// <summary>
+    /// Director interface.
+    /// </summary>
+    /// <typeparam name="T">Builder generic type.</typeparam>
+    public interface IDirector<T>
+    {
+        /// <summary>
+        /// Builder.
+        /// </summary>
+        T Builder { get; }
+
+        /// <summary>
+        /// Build minimal viable product.
+        /// </summary>
+        public Task BuildMinimalViableProduct();
+
+        /// <summary>
+        /// Build full featured product.
+        /// </summary>
+        public Task BuildFullFeaturedProduct();
+    }
+}
