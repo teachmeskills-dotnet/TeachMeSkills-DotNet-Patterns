e8e77d11c861b3953c80e3fbb5f970bd0279cf25
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 19:55:16 +0300

    feat: add behavioral pattern - observer

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Observer/IObserver.cs
@@ -0,0 +1,14 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Observer
+{
+    /// <summary>
+    /// Observer interface.
+    /// </summary>
+    public interface IObserver
+    {
+        /// <summary>
+        /// Update.
+        /// </summary>
+        /// <param name="subject">Subject.</param>
+        public void Update(IObserverSubject subject);
+    }
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Observer/IObserverSubject.cs
@@ -0,0 +1,25 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Observer
+{
+    /// <summary>
+    /// Subject interface.
+    /// </summary>
+    public interface IObserverSubject
+    {
+        /// <summary>
+        /// Attach.
+        /// </summary>
+        /// <param name="observer">Observer.</param>
+        public void Attach(IObserver observer);
+
+        /// <summary>
+        /// Detach.
+        /// </summary>
+        /// <param name="observer">Observer.</param>
+        public void Detach(IObserver observer);
+
+        /// <summary>
+        /// Notify.
+        /// </summary>
+        public void Notify();
+    }
+}
