e766dad384fe44f2b56958ba2de73af268710026
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 19:52:23 +0300

    feat: add behavioral pattern - memento

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Memento/IMemento.cs
@@ -0,0 +1,25 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Memento
+{
+    /// <summary>
+    /// Memento interface.
+    /// </summary>
+    /// <typeparam name="T">State generic type.</typeparam>
+    /// <typeparam name="V">Method return generic type.</typeparam>
+    public interface IMemento<T, V>
+    {
+        /// <summary>
+        /// State.
+        /// </summary>
+        public T State { get; }
+
+        /// <summary>
+        /// Date.
+        /// </summary>
+        public DateTime Date { get; }
+
+        /// <summary>
+        /// Get name.
+        /// </summary>
+        V GetName();
+    }
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Memento/IMementoCaretaker.cs
@@ -0,0 +1,36 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Memento
+{
+    /// <summary>
+    /// Memento caretaker interface.
+    /// </summary>
+    /// <typeparam name="T">State generic type.</typeparam>
+    /// <typeparam name="V">Method return generic type.</typeparam>
+    /// <typeparam name="Z">Originator generic type.</typeparam>
+    public interface IMementoCaretaker<T, V, Z>
+    {
+        /// <summary>
+        /// Mementos.
+        /// </summary>
+        public List<IMemento<T, V>> Mementos { get; }
+
+        /// <summary>
+        /// Originator.
+        /// </summary>
+        public Z Originator { get; }
+
+        /// <summary>
+        /// Backup.
+        /// </summary>
+        public void Backup();
+
+        /// <summary>
+        /// Undo.
+        /// </summary>
+        public void Undo();
+
+        /// <summary>
+        /// ShowHistory.
+        /// </summary>
+        public void ShowHistory();
+    }
+}
