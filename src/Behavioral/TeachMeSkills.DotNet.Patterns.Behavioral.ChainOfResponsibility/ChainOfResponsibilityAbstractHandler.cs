7547c1aaa22470980bbb3eb060414997af9d199d
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 19:34:45 +0300

    feat: add behavioral pattern - chain of responsibility

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.ChainOfResponsibility/ChainOfResponsibilityAbstractHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.ChainOfResponsibility
+{
+    /// <summary>
+    /// Abstract chain of responsibility handler.
+    /// </summary>
+    public abstract class ChainOfResponsibilityAbstractHandler : IChainOfResponsibilityHandler
+    {
+        /// <summary>
+        /// Next handler.
+        /// </summary>
+        private IChainOfResponsibilityHandler _nextHandler;
+
+        /// <summary>
+        /// Set next handler.
+        /// </summary>
+        /// <param name="handler">Next handler.</param>
+        public IChainOfResponsibilityHandler SetNext(IChainOfResponsibilityHandler handler)
+        {
+            _nextHandler = handler ?? throw new ArgumentNullException(nameof(handler));
+
+            return handler;
+        }
+
+        /// <summary>
+        /// Handle.
+        /// </summary>
+        /// <param name="request">Request.</param>
+        public virtual object Handle(object request)
+        {
+            return _nextHandler != null 
+                ? _nextHandler.Handle(request) 
+                : null;
+        }
+    }
+
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.ChainOfResponsibility/IChainOfResponsibilityHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.ChainOfResponsibility
+{
+    /// <summary>
+    /// Chain of responsibility interface.
+    /// </summary>
+    public interface IChainOfResponsibilityHandler
+    {
+        /// <summary>
+        /// Set next handler.
+        /// </summary>
+        /// <param name="handler">Next handler.</param>
+        IChainOfResponsibilityHandler SetNext(IChainOfResponsibilityHandler handler);
+
+        /// <summary>
+        /// Handle.
+        /// </summary>
+        /// <param name="request">Request.</param>
+        object Handle(object request);
+    }
+}
