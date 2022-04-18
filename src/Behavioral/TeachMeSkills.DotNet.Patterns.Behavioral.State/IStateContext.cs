8bb19c02e6c6aa0d680d8f7310a4593431a89790
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 19:58:40 +0300

    feat: add behavioral pattern - state

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.State/IStateContext.cs
@@ -0,0 +1,24 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.State
+{
+    /// <summary>
+    /// State context interface.
+    /// </summary>
+    public interface IStateContext
+    {
+        /// <summary>
+        /// State.
+        /// </summary>
+        public StateBase State { get; }
+
+        /// <summary>
+        /// Transition to new state.
+        /// </summary>
+        /// <param name="state">New state.</param>
+        public void TransitionTo(StateBase state);
+
+        /// <summary>
+        /// First request.
+        /// </summary>
+        public void Request1();
+    }
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.State/StateBase.cs
@@ -0,0 +1,27 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.State
+{
+    /// <summary>
+    /// State base class.
+    /// </summary>
+    public abstract class StateBase
+    {
+        /// <summary>
+        /// Context.
+        /// </summary>
+        protected IStateContext _context;
+
+        /// <summary>
+        /// Set new context.
+        /// </summary>
+        /// <param name="context">New context.</param>
+        public void SetContext(IStateContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// First handler.
+        /// </summary>
+        public abstract void Handle1();
+    }
+}
