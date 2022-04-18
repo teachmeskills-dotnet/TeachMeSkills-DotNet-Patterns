13e41dea214c7dff8a2d764500b4e89196228543
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 19:44:41 +0300

    feat: add behavioral pattern - mediator

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Mediator/IMediator.cs
@@ -0,0 +1,16 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Mediator
+{
+    /// <summary>
+    /// Mediator interface.
+    /// </summary>
+    /// <typeparam name="T">Data generic type.</typeparam>
+    public interface IMediator<T>
+    {
+        /// <summary>
+        /// Notify.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="param">Data.</param>
+        public void Notify(object sender, T param);
+    }
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Mediator/MediatorBaseComponent.cs
@@ -0,0 +1,31 @@
+﻿using TeachMeSkills.DotNet.Patterns.Behavioral.Mediator;
+
+namespace TeachMeSkills.Patterns.Mediator
+{
+    /// <summary>
+    /// Mediator base component.
+    /// </summary>
+    /// <typeparam name="T">Data generic type.</typeparam>
+    public abstract class MediatorBaseComponent<T>
+    {
+        protected IMediator<T> _mediator;
+
+        /// <summary>
+        /// Constructor with params.
+        /// </summary>
+        /// <param name="mediator">Mediator.</param>
+        public MediatorBaseComponent(IMediator<T> mediator = null)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Set mediator.
+        /// </summary>
+        /// <param name="mediator">Mediator.</param>
+        public void SetMediator(IMediator<T> mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+    }
+}
