be44df9e5b4477b69eb8d2a2dae9b7b5d0609ca6
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 20:08:29 +0300

    feat: add behavioral pattern - visitor

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Visitor/IVisitorComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Visitor
+{
+    /// <summary>
+    /// Visitor component interface.
+    /// </summary>
+    /// <typeparam name="T">Use custom IVisitor interface.</typeparam>
+    public interface IVisitorComponent<T>
+    {
+        /// <summary>
+        /// Accept.
+        /// </summary>
+        /// <param name="visitor">Visitor.</param>
+        public void Accept(T visitor);
+    }
+}
