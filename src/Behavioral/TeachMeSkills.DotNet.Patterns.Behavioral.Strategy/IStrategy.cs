be476ec15e423bfe00258bd1c44bba9fd8654d30
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 20:03:25 +0300

    feat: add behavioral pattern - strategy

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Strategy/IStrategy.cs
@@ -0,0 +1,14 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Stategy
+{
+    /// <summary>
+    /// Stategy interface.
+    /// </summary>
+    public interface IStrategy
+    {
+        /// <summary>
+        /// Do.
+        /// </summary>
+        /// <param name="data">Data.</param>
+        object Do(object data);
+    }
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Strategy/IStrategyContext.cs
@@ -0,0 +1,24 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Stategy
+{
+    /// <summary>
+    /// Strategy context interface.
+    /// </summary>
+    public interface IStrategyContext
+    {
+        /// <summary>
+        /// Strategy.
+        /// </summary>
+        public IStrategy Strategy { get; }
+
+        /// <summary>
+        /// Set strategy.
+        /// </summary>
+        /// <param name="strategy">Strategy.</param>
+        public void SetStrategy(IStrategy strategy);
+
+        /// <summary>
+        /// Do.
+        /// </summary>
+        public void Do();
+    }
+}
