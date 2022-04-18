fd11dafa677e96b071dbe033b4bff8be40455908
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2022-04-18 19:37:37 +0300

    feat: add behavioral pattern - command

---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Command/ICommand.cs
@@ -0,0 +1,15 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Command
+{
+    // UNDONE: change void to T
+
+    /// <summary>
+    /// Command interface.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Execute.
+        /// </summary>
+        public void Execute();
+    }
+}
---
+++ src/Behavioral/TeachMeSkills.DotNet.Patterns.Behavioral.Command/ICommandInvoker.cs
@@ -0,0 +1,21 @@
+﻿namespace TeachMeSkills.DotNet.Patterns.Behavioral.Command
+{
+    // UNDONE: change void to T
+
+    /// <summary>
+    /// Command invoker interface.
+    /// </summary>
+    public interface ICommandInvoker
+    {
+        /// <summary>
+        /// Set first command.
+        /// </summary>
+        /// <param name="command">Command interface.</param>
+        public void Set1(ICommand command);
+
+        /// <summary>
+        /// Do.
+        /// </summary>
+        public void Do();
+    }
+}
